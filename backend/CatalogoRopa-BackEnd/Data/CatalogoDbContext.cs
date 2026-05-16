c98bdb0bf41827db5f3d2981ac1461832057e7b2
JPGE99 <jpge_lazar@hotmail.com>
2026-05-15 21:03:43 -0700

    Fix: Se actualizaron los modelos.
    Se actualizo el modelo de Ropa a Producto y se agregaron el resto de modelos necesarios para el proyecto.

--- backend/CatalogoRopa-BackEnd/Data/CatalogoDbContext.cs
+++ backend/CatalogoRopa-BackEnd/Data/CatalogoDbContext.cs
@@ -10,13 +10,48 @@ namespace CatalogoRopa_BackEnd.Data
         {
         }
 
-        public DbSet<Ropa> Ropa { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Coleccion> Coleccion { get; set; }
+        public DbSet<Promocion> Promocion { get; set; }
+        public DbSet<ImagenProducto> ImagenProducto { get; set; }
+        public DbSet<Variante> Variante { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Favorito> Favorito { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Ropa>()
-                .Property(r => r.Precio)
-                .HasPrecision(10, 2);
+            modelBuilder.Entity<Categoria>()
+            .HasOne(c => c.CategoriaPadreNavigation)
+            .WithMany(c => c.Subcategorias)
+            .HasForeignKey(c => c.CategoriaPadre)
+            .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Favorito>()
+                .HasIndex(f => new { f.IdUsuario, f.IdProducto })
+                .IsUnique();
+
+            modelBuilder.Entity<Producto>()
+                .HasOne(p => p.Marca)
+                .WithMany(m => m.Productos)
+                .HasForeignKey(p => p.IdMarca);
+
+            modelBuilder.Entity<Producto>()
+                .HasOne(p => p.Categoria)
+                .WithMany(c => c.Productos)
+                .HasForeignKey(p => p.IdCategoria);
+
+            modelBuilder.Entity<Producto>()
+                .HasOne(p => p.Coleccion)
+                .WithMany(c => c.Productos)
+                .HasForeignKey(p => p.IdColeccion);
+
+            modelBuilder.Entity<Producto>()
+                .HasOne(p => p.Promocion)
+                .WithMany(p => p.Productos)
+                .HasForeignKey(p => p.IdPromocion);
         }
     }
 }
