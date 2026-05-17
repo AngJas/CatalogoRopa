0045cbaa31254d26f8e35bf2f36ced22d107c290
JPGE99 <jpge_lazar@hotmail.com>
2026-05-17 11:33:44 -0700

    Feat: Se agrego el componente footer.
    se agrego el componente footer al app.component.html para que aparezca en todas las paginas.

--- frontend/CatalogoRopa-FrontEnd/src/app/app.ts
+++ frontend/CatalogoRopa-FrontEnd/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, RouterLink],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
