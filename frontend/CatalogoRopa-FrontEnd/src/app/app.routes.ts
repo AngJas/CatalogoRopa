527508399482858a6bdfcca23a2b0189a199f037
JPGE99 <jpge_lazar@hotmail.com>
2026-05-17 06:38:18 -0700

    Feat: Se agrego la ruta para agregar un nuevo producto al catalogo

--- frontend/CatalogoRopa-FrontEnd/src/app/app.routes.ts
+++ frontend/CatalogoRopa-FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CatalogoComponent } from './pages/catalogo/catalogo';
 import { InicioComponent } from './pages/inicio/inicio';
+import { AgregarProductoComponent } from './agregar-producot/agregar-producot.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,11 @@ export const routes: Routes = [
   {
     path: 'catalogo',
     component: CatalogoComponent
+  },
+
+  {
+    path: 'producto/nuevo',
+    component: AgregarProductoComponent
   }
 
 ];
