ae732c073081f5d2a0dafde3620b6bd8c7fa9030
JPGE99 <jpge_lazar@hotmail.com>
2026-05-17 00:52:05 -0700

    Fix: Se  modific el componente de inicio.
    Se agregaron los componentes de banner promocional y catalogo al componente de inicio, ademas se modifico el html del componente de inicio para mostrar ambos componentes.

--- frontend/CatalogoRopa-FrontEnd/src/app/pages/inicio/inicio.ts
+++ frontend/CatalogoRopa-FrontEnd/src/app/pages/inicio/inicio.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BannerPromocionalComponent } from "../banner-promocional/banner-promocional.component";
+import { CatalogoComponent } from "../catalogo/catalogo";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, BannerPromocionalComponent, CatalogoComponent],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
