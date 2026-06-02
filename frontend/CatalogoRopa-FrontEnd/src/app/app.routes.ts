d4b5428b68acc8d2bc6d0bf52b75726d29f3f116
JPGE99 <jpge_lazar@hotmail.com>
2026-06-01 17:54:18 -0700

    Fix: Rutas de navegacion actualizadas.
    se actualizaron las rutas de navegacion del header, estas mandan correctamente al usuario al catalogo de la respectiva categoria.

--- frontend/CatalogoRopa-FrontEnd/src/app/app.routes.ts
+++ frontend/CatalogoRopa-FrontEnd/src/app/app.routes.ts
@@ -15,18 +15,52 @@ export const routes: Routes = [
   },
 
   {
+    path: 'catalogo/:seccion',
+    component: CatalogoComponent
+  },
+
+  {
+    path: 'novedades',
+    redirectTo: 'catalogo/novedades',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'promociones',
+    redirectTo: 'catalogo/promociones',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'hombre',
+    redirectTo: 'catalogo/hombre',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'mujer',
+    redirectTo: 'catalogo/mujer',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'todo',
+    redirectTo: 'catalogo/todo',
+    pathMatch: 'full'
+  },
+
+  {
     path: 'producto/nuevo',
     component: AgregarProductoComponent
-  }
+  },
 
-  ,
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
   },
+
   {
     path: 'register',
     loadComponent: () => import('./pages/register/register').then(m => m.RegisterComponent)
   }
-
 ];
